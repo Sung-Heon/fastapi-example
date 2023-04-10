d6f10dd95f262c061fe4364984314e4e8e239582
elon <elon4856@gmail.com>
2023-04-10 14:43:02 +0900

    feat
    uvicorn 추가.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -6,5 +7,8 @@ app = FastAPI()
 @app.get("/hello")
 def hello():
     return {"message": "안녕하세요 슈퍼코딩"}
-    
 
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', port=8000, reload=True)
