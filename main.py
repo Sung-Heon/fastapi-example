ac832ffb246dd88adf80208716779e370c469adb
elon <elon4856@gmail.com>
2023-04-10 17:44:12 +0900

    query_param 추가.

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def hello():
     return {"message": "안녕하세요 슈퍼코딩"}
 
 
+@app.get("/items/{item_id}")
+def read_item(item_id):
+    return {"item_id": item_id}
+
 
 if __name__ == '__main__':
     uvicorn.run('main:app', port=8000, reload=True)
