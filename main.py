144221566330436217f060fe292e1e25189b71f6
elon <elon4856@gmail.com>
2023-04-10 18:08:42 +0900

    query_param

--- main.py
+++ main.py
@@ -18,5 +18,12 @@ def read_item(item_id):
 def read_item_int_only(item_id:int):
     return {"int-item_id": item_id}
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
 if __name__ == '__main__':
     uvicorn.run('main:app', port=8000, reload=True)
